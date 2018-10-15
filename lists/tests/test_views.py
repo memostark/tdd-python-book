2aa6bd00bdc22c4a96f0d5f35076206bcb47570a
Memo <memito_095@hotmail.com>
2018-10-14 23:36:26 -0500

    Added validation for an existing list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,6 +8,8 @@ from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
+from unittest import skip
+
 class HomePageTest(TestCase):
 
 	def test_home_page_returns_correct_html(self):
@@ -71,6 +73,12 @@ class ListsViewTest(TestCase):
 
 		self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
+	def test_displays_item_form(self):
+		list_ = List.objects.create()
+		response = self.client.get(f'/lists/{list_.id}/')
+		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertContains(response, 'name="text"')
+
 	def post_invalid_input(self):
 		list_ = List.objects.create()
 		return self.client.post(
@@ -95,11 +103,20 @@ class ListsViewTest(TestCase):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-	def test_displays_item_form(self):
-		list_ = List.objects.create()
-		response = self.client.get(f'/lists/{list_.id}/')
-		self.assertIsInstance(response.context['form'], ItemForm)
-		self.assertContains(response, 'name="text"')
+	@skip
+	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(
+			f'/lists/{list1.id}/',
+			data={'text': 'textey'}
+		)
+
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.all().count(), 1)
+
 
 class NewListTest(TestCase):
 
